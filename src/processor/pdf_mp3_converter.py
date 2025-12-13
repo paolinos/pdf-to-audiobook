0f55ce9495d1b4136672e28d46b7ca3ca3e38db4
Pablo <pendlejin@yahoo.com.ar>
2025-12-13 01:57:07 +0100

    chore: update pdf_mp3_converter with references

--- src/processor/pdf_mp3_converter.py
+++ src/processor/pdf_mp3_converter.py
@@ -1,25 +1,23 @@
-
-from os import path
 import time
+from os import path
 from pathlib import Path
 
-from helpers.file import get_file_data, read_file_async, write_file_async, delete_file
-from pdf_converter.markdown import convert_pdf_to_txt
-from tts.tts import TextToAudio
 from ffmpeg.join import merge_and_convert_mp3
+from helpers.file import delete_file, get_file_data, read_file_async, write_file_async
+from pdf_converter.markdown import convert_pdf_to_txt
+from tts.coqui_tts import CoquiTTS
 
 TEMP_FOLDER = "./tmp"
 
 
 class PdfMp3Converter:
-
     def __init__(self):
         self._check_tmp_folder(TEMP_FOLDER)
 
     async def _pdf_to_text(self, pdf_path: str, force: bool = False) -> str:
         """
         Read pdf file and convert to text file.
-        
+
         :param pdf_path: path of the pdf file to convert
         :type pdf_path: str
         :param force: force to recreate. Default option False
@@ -28,7 +26,7 @@ class PdfMp3Converter:
         :rtype: str
         """
 
-        (folder, name, _) = get_file_data(pdf_path)
+        (_, name, _) = get_file_data(pdf_path)
         output_file = path.join(TEMP_FOLDER, f"{name}.txt")
         if path.isfile(output_file) and force is False:
             return output_file
@@ -42,7 +40,7 @@ class PdfMp3Converter:
         Read text file and create the wav files
         We're going to open the markdown file, and generate N audio files.
         This is to make less intensive and use less VRAM & GPU.
-        
+
         :param text_path: text file path
         :type text_path: str
         :param force: force to recreate. Default option False
@@ -53,10 +51,11 @@ class PdfMp3Converter:
         text_content = await read_file_async(text_path)
         (folder, name, _) = get_file_data(text_path)
         blocks = text_content.split("\n\n")
-        print(f"text_path:{text_path}, folder:{folder}, name:{name}, blocks:{len(blocks)}")
+        print(
+            f"text_path:{text_path}, folder:{folder}, name:{name}, blocks:{len(blocks)}"
+        )
 
-
-        audiogen = TextToAudio()
+        audiogen = CoquiTTS()
 
         audios: list[str] = []
         i = 0
@@ -77,21 +76,19 @@ class PdfMp3Converter:
 
         return audios
 
-    def _check_tmp_folder(self, path:str):
+    def _check_tmp_folder(self, path: str):
         """
         Docstring for check_tmp_folder
-        
+
         :param path: Description
         :type path: str
         """
         Path(path).mkdir(parents=True, exist_ok=True)
-    
 
-
-    def _validate(self, pdf_path:str) -> bool:
+    def _validate(self, pdf_path: str) -> bool:
         """
         Docstring for _validate
-        
+
         :param pdf_path: pdf path
         :type pdf_path: str
         :return:
@@ -99,28 +96,27 @@ class PdfMp3Converter:
         """
         (_, _, ext) = get_file_data(pdf_path)
         return path.isfile(pdf_path) and ext.lower() == ".pdf"
-    
 
-    def _delete_temp_files(self, tmp_paths:list[str]):
+    def _delete_temp_files(self, tmp_paths: list[str]):
         for fp in tmp_paths:
             delete_file(fp)
 
-
-
-    async def run(self, pdf_path:str):
+    async def run(self, pdf_path: str):
         dt = time.time()
 
         if self._validate(pdf_path) is False:
-            raise ValueError("Error: Invalid pdf file or path. Please review the path of the pdf file.")
+            raise ValueError(
+                "Error: Invalid pdf file or path. Please review the path of the pdf file."
+            )
 
-        tmp_paths:list[str] = []
+        tmp_paths: list[str] = []
 
         text_path = await self._pdf_to_text(pdf_path)
         tmp_paths.append(text_path)
 
         audios = await self._text_to_audio(text_path)
-        tmp_paths  = tmp_paths + audios
-        
+        tmp_paths = tmp_paths + audios
+
         merge_and_convert_mp3(audios, pdf_path)
 
         # Delete files
